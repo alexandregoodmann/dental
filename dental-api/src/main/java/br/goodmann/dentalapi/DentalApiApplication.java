364217f62360d7325757cf6f294bb55c60b51236
Alexandre <alexandregoodmann@gmail.com>
2018-03-24 14:25:25 -0300

    initial

---
+++ dental-api/src/main/java/br/goodmann/dentalapi/DentalApiApplication.java
@@ -0,0 +1,12 @@
+package br.goodmann.dentalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalApiApplication.class, args);
+	}
+}
